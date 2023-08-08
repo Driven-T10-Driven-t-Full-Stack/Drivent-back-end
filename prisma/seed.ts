733ca4101d69dba1e535089a047c79d7f4e08755
Lúcio Bruno <luciobrunop20@hotmail.com>
2023-08-08 15:50:35 -0400

    feat: add activity on database and seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,7 +15,50 @@ async function main() {
       },
     });
   }
+  const eventActivities = [
+    {
+      name: "Navegando no Mundo da Inteligência Artificial: Desmitificando Conceitos Complexos",
+      startedTime: dayjs('2023-08-10').add(28, "days").set('hour', 10).toDate(),
+      finishedTime: dayjs('2023-08-10').add(28, "days").set('hour', 12).toDate(),
+      local: "LATERAL",
+      capacity: 25,
+      eventId: 1, 
+    },
+    {
+      name: "Construindo Resiliência em Tempos de Mudança: Estratégias para Lidar com Desafios",
+      startedTime: dayjs('2023-08-10').add(29, "days").set('hour', 10).toDate(),
+      finishedTime: dayjs('2023-08-10').add(29, "days").set('hour', 12).toDate(),
+      local: 'PRINCIPAL',
+      capacity: 25,
+      eventId: 1, 
+    },
+    {
+      name: "Sustentabilidade na Prática: Iniciativas Empresariais para um Futuro Mais Verde",
+      startedTime: dayjs('2023-08-10').add(30, "days").set('hour', 10).toDate(),
+      finishedTime: dayjs('2023-08-10').add(30, "days").set('hour', 12).toDate(),
+      local: 'WORKSHOP',
+      capacity: 25,
+      eventId: 1, 
+    }
+  ]
+  let activities = await prisma.activity.findMany();
+  if (activities.length === 0) {
+    for(let i=0; i<eventActivities.length;i++){
+      const activity = await prisma.activity.create({
+        data: {
+          name: eventActivities[i].name,
+          startedTime: eventActivities[i].startedTime,
+          finishedTime: eventActivities[i].finishedTime,
+          local: eventActivities[i].local,
+          capacity: eventActivities[i].capacity,
+          eventId: eventActivities[i].eventId,
+  },
+      });
+      activities = [...activities, activity]
+    }
+  }
 
+  console.log({ activities });
   console.log({ event });
 }
 
